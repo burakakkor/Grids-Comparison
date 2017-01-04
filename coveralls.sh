d8758ff6a08860a1ab1e2d2c2b50dba4710023f3
burakakkor <burakakkor@gmail.com>
2017-01-04 17:25:13 +0000

    updated coveralls.sh

--- coveralls.sh
+++ coveralls.sh
@@ -2,6 +2,6 @@
 
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
-  nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
-  packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i coverage/coverage.xml --useRelativePaths
+  nuget install -OutputDirectory Grids/packages -Version 0.7.0 coveralls.net
+  Grids/packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i coverage/coverage.xml --useRelativePaths
 fi
